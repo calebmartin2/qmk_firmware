c1292f75080de093f9f1c1b0f1dd0a4bc2d587ef
calebmartin2 <me@caleb.net>
2025-07-16 20:58:15 -0500

    add krita layer

--- keyboards/cheapino/keymaps/cm2json/keymap.c
+++ keyboards/cheapino/keymaps/cm2json/keymap.c
@@ -1,40 +1,51 @@
-#include "wait.h"
 #include "quantum.h"
+#include "socd_cleaner.h"
+#include "sentence_case.h"
+//#include "orbital_mouse.h"
 
 socd_cleaner_t socd_opposing_pairs[] = {
-  {{KC_W, KC_S}, SOCD_CLEANER_LAST},
-  {{KC_A, KC_D}, SOCD_CLEANER_LAST},
+    {{KC_W, KC_S}, SOCD_CLEANER_LAST},
+    {{KC_A, KC_D}, SOCD_CLEANER_LAST},
 };
 
 bool sentence_case_persist;
 bool socd_cleaner_persist = true;
 
-const uint8_t HUE_RED = 85;
+const uint8_t HUE_RED   = 85;
 const uint8_t HUE_GREEN = 0;
-const uint8_t HUE_BLUE = 170;
+const uint8_t HUE_BLUE  = 170;
+
+const uint8_t LAYER_GAMING = 7;
+const uint8_t LAYER_GAMING_NUMBERS = 8;
+const uint8_t LAYER_KRITA = 9;
+const uint8_t LAYER_BLANK = 10;
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     if (record->event.pressed) {
         switch (keycode) {
             case QK_MACRO_0:
-                    SEND_STRING(" => ");
+                SEND_STRING(" => ");
                 break;
             case QK_MACRO_1:
-                    SEND_STRING("/>");
+                SEND_STRING("/>");
                 break;
-            case QK_MACRO_2:  // Next sentence macro.
-                    SEND_STRING(". ");
-                    add_oneshot_mods(MOD_BIT(KC_LSFT));  // Set one-shot mod for shift.
+            case QK_MACRO_2: // Next sentence macro.
+                SEND_STRING(". ");
+                add_oneshot_mods(MOD_BIT(KC_LSFT)); // Set one-shot mod for shift.
                 break;
             case QK_MACRO_3:
-                    sentence_case_toggle();
-                    sentence_case_persist = is_sentence_case_on();
+                sentence_case_toggle();
+                sentence_case_persist = is_sentence_case_on();
                 break;
             case QK_MACRO_4:
-                    socd_cleaner_enabled = !socd_cleaner_enabled;
-                    rgblight_sethsv_noeeprom(socd_cleaner_enabled ? HUE_RED : HUE_BLUE, 255, 1);
-                    socd_cleaner_persist = socd_cleaner_enabled;
+                socd_cleaner_enabled = !socd_cleaner_enabled;
+                rgblight_sethsv_noeeprom(socd_cleaner_enabled ? HUE_RED : HUE_BLUE, 255, 1);
+                socd_cleaner_persist = socd_cleaner_enabled;
                 break;
+            // case QK_MACRO_5:
+            //     SEND_STRING(SS_LGUI("2"));
+            //     layer_move(GAMING);
+            //     break;
         }
     }
     return true;
@@ -44,12 +55,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // initial RGB flash is finished
 uint8_t _hue_countdown = 50;
 
-// These are to keep track of user selected color, so we
-// can restore it after RGB flash
-// uint8_t _hue;
-// uint8_t _saturation;
-// uint8_t _value;
-
 // Do a little 2.5 seconds display of the different colors
 // Use the deferred executor so the LED flash dance does not
 // stop us from using the keyboard.
@@ -58,7 +63,6 @@ uint32_t flash_led(uint32_t next_trigger_time, void *cb_arg) {
     rgblight_sethsv_noeeprom(_hue_countdown * 5, 230, 70);
     _hue_countdown--;
     if (_hue_countdown == 0) {
-        // Finished, reset to user chosen led color
         rgblight_sethsv_noeeprom(0, 255, 0);
         return 0;
     } else {
@@ -72,20 +76,19 @@ void keyboard_post_init_user(void) {
     // debug_keyboard=true;
     // debug_mouse=true;
 
-    // Store user selected rgb hsv:
-    // _hue        = rgblight_get_hue();
-    // _saturation = rgblight_get_sat();
-    // _value      = rgblight_get_val();
-    sentence_case_off();
+    rgblight_sethsv(0,0,0);
+    sentence_case_on();
     // Flash a little on start
     defer_exec(50, flash_led, NULL);
 }
 
 uint8_t get_hue(uint8_t layer) {
     switch (layer) {
-        case 8:
+        case LAYER_KRITA:
+        case LAYER_BLANK:
+        case LAYER_GAMING_NUMBERS:
             return HUE_GREEN;
-        case 7:
+        case LAYER_GAMING:
             return socd_cleaner_enabled ? HUE_RED : HUE_BLUE;
         default:
             return 0;
@@ -94,8 +97,10 @@ uint8_t get_hue(uint8_t layer) {
 
 uint8_t get_val(uint8_t layer) {
     switch (layer) {
-        case 8:
-        case 7:
+        case LAYER_BLANK:
+        case LAYER_KRITA:
+        case LAYER_GAMING_NUMBERS:
+        case LAYER_GAMING:
             return 1;
         default:
             return 0;
@@ -110,9 +115,9 @@ layer_state_t layer_state_set_user(layer_state_t state) {
         socd_cleaner_enabled = false;
     }
 
-    uint8_t sat          = rgblight_get_sat();
-    uint8_t val          = get_val(get_highest_layer(state));
-    uint8_t hue          = get_hue(get_highest_layer(state));
+    uint8_t sat = rgblight_get_sat();
+    uint8_t val = get_val(get_highest_layer(state));
+    uint8_t hue = get_hue(get_highest_layer(state));
     rgblight_sethsv_noeeprom(hue, sat, val);
     return state;
 }
