c5ad42ee57342e9daf4df2097a0e7f710437f88b
calebmartin2 <me@caleb.net>
2026-01-13 22:06:06 -0600

    run qmk migrate

--- keyboards/cheapino/config.h
+++ keyboards/cheapino/config.h
@@ -23,14 +23,3 @@
 #define WS2812_PIO_USE_PIO1 // Force the usage of PIO1 peripheral, by default the WS2812 implementation uses the PIO0 peripheral
 //#define WS2812_TRST_US 80
 #define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
-
-#define WS2812_DI_PIN GP16 // The pin connected to the data pin of the LEDs
-#define RGBLIGHT_LED_COUNT 1                     // The number of LEDs connected
-
-
-// #define DEBUG_MATRIX_SCAN_RATE
-
-#define RGBLIGHT_DEFAULT_HUE 128 // Sets the default hue value, if none has been set
-#define RGBLIGHT_DEFAULT_SAT 128 // Sets the default saturation value, if none has been set
-#define RGBLIGHT_DEFAULT_VAL 32 // Sets the default brightness value, if none has been set
-                                   //
